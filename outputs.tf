0619220eeb17f7614d9bee3d9130e720a68ff6cd
Kawsar Kamal <kawsar@hashicorp.com>
2018-07-06 14:09:43 -0400

    Removed output azurerm_public_ip.vm.*.ip_address
    Removed output azurerm_public_ip.vm.*.ip_address due to eventual consistency error

--- outputs.tf
+++ outputs.tf
@@ -23,11 +23,6 @@ output "public_ip_id" {
   value       = "${azurerm_public_ip.vm.*.id}"
 }
 
-output "public_ip_address" {
-  description = "The actual ip address allocated for the resource."
-  value       = "${azurerm_public_ip.vm.*.ip_address}"
-}
-
 output "public_ip_dns_name" {
   description = "fqdn to connect to the first vm provisioned."
   value       = "${azurerm_public_ip.vm.*.fqdn}"
